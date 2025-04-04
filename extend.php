84e1a41feec020b3fb5e2b1571a35314c63650ef
jgrowlow <128536296+jgrowlow@users.noreply.github.com>
2025-04-04 13:30:14 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -3,6 +3,7 @@ use Flarum\Extend;
 use Flarum\Http\Middleware\CheckCsrfToken;
 use CustomApi\Controllers\LoginController;
 use CustomApi\Providers\CustomApiServiceProvider;
+use CustomApi\Auth\UserAuthenticator;
 
 return [
     (new Extend\Routes('api'))
@@ -11,8 +12,13 @@ return [
     (new Extend\ServiceProvider())
         ->register(CustomApiServiceProvider::class),
 
+    // Register the custom UserAuthenticator as a service
+    (new Extend\ServiceProvider())
+        ->register(function () {
+            return new UserAuthenticator();
+        }),
+
     // Add CheckCsrfToken middleware to the API pipeline
     (new Extend\Middleware('api'))
         ->add(CheckCsrfToken::class),
 ];
-
