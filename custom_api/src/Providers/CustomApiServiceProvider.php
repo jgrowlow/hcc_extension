1691ac1ac21858603326781d1a01c25b3c8f2b53
jgrowlow <128536296+jgrowlow@users.noreply.github.com>
2025-04-04 11:48:10 -0400

    Create CustomApiServiceProvider.php

---
+++ custom_api/src/Providers/CustomApiServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace CustomApi\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CustomApiServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register services here
+    }
+
+    public function boot()
+    {
+        // Boot services here
+    }
+}
